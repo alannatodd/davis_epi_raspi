11f6d33be04b508b9d9e4052662b04733d6de7c2
alannatodd <alanna.g.todd@gmail.com>
2025-01-12 10:37:37 -0800

    adding comments, generalizing var names

--- continuous.py
+++ continuous.py
@@ -10,40 +10,56 @@ import shutil
 from threading import Thread
 import cv2
 
-# 20 minutes
-seconds = 1200
+#### NOTE: This file should be placed in the /code directory ####
 
-# Flash drive setup
-flash_path = '/media/fishpi10/DUAL DRIVE/'
+#### PI / FILE STRUCTURE SETUP ####
+pi_username = os.getlogin() # ie, davis_epi_raspi1
+
+# Modify the following variables to match the individual pi's setup
+# This subdirectory must exist under the /code directory
+subdir = "goats"
+# Verify the appearance of the drive's name when connected to the pi 
+usb_name = "DUAL DRIVE"
+
+#### FLASH DRIVE SETUP ####
+flash_path = f"/media/{pi_username}/{usb_name}/"
 to_copy = []
-username = os.getlogin()
 
-# Timestamp overlay options
+#### TIMESTAMP OVERLAY OPTIONS ####
 color = (0, 255, 0)
 origin = (0, 30)
 font = cv2.FONT_HERSHEY_SIMPLEX
 scale = 1
 thickness = 2
 
-# camera setup
+#### CAMERA SETUP ####
 camera = Picamera2()
-#video_config = camera.create_video_configuration()
+
+# Set the video size and display type
 video_config = camera.create_video_configuration(main={"size":(480,480)}, lores={"size":(360,360)}, display="lores")
 camera.configure(video_config)
 encoder = H264Encoder(10000000)
-#camera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-
 keep_running = True
 
-error_file = f"fish/errors_{datetime.now().strftime('%Y-%m-%d_%H.%M')}.txt"
+# The length of each video in seconds (60 seconds * 20 minutes = 1200)
+minutes = 20
+seconds = minutes * 60
+
+## Uncomment to turn on continuous autofocus; Keep commented out to turn off autofocus
+#camera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+# Create a file to store any logged errors
+error_file = f"{subdir}/errors_{datetime.now().strftime('%Y-%m-%d_%H.%M')}.txt"
 with open(error_file, 'x') as f:
     f.write("Error log:\n")
 
+# A function to app a timestamp overlay on the video using the settings set in TIMESTAMP OVERLAY OPTIONS section
 def apply_timestamp(request):
     timestamp = time.strftime("%Y-%m-%d %X")
     with MappedArray(request, "main") as m:
         cv2.putText(m.array, timestamp, origin, font, scale, color, thickness)
 
+# Function to copy files to the flash drive
 def copy_files():
     global to_copy
     
@@ -54,6 +70,8 @@ def copy_files():
             shutil.copyfile(to_copy[i], flash_file_path)
         except Exception as e:
             # write error to error logs
+            # The video file will not be removed from the pi if it was not successfully copied,
+            # but the program will not try to copy it to the flash drive again
             global error_file
             with open(error_file, 'a') as f:
                 f.write(f'{datetime.now()} {e}')
@@ -61,7 +79,7 @@ def copy_files():
             to_copy = to_copy[i:]
             return
         
-        # If successfully copied to flash drive, remove from SD card
+        # If successfully copied to flash drive, remove the video file from the pi
         os.remove(to_copy[i])
         
     # Remove file names from to_copy
@@ -72,15 +90,16 @@ camera.pre_callback = apply_timestamp
 while keep_running:
     try:
         # Start video recording
-        sd_file_path = f"fish/fishpi10_{datetime.now().strftime('%Y-%m-%d_%H.%M.%S')}_20min.mp4"
+        sd_file_path = f"{subdir}/{username}_{datetime.now().strftime('%Y-%m-%d_%H.%M.%S')}_{minutes}min.mp4"
         camera.start_and_record_video(sd_file_path, duration=seconds)
         to_copy.append(sd_file_path)
         
-        # Copy file to flash drive if connected as a thread so next file recording is not blocked
+        # Use a thread to copy video file to flash drive (if drive connected) so next file recording is not blocked
         t = Thread(target=copy_files)
         t.start()
 
     except Exception as e:
+        # Write any errors to the error log file
         with open(error_file, 'a') as f:
                 f.write(f'{datetime.now()} {e}')
 
